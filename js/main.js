ce65dba81f7909a9db512af613996ef23d70c4a4
chris mears <chrismears@chriss-mbp.gateway.pace.com>
2017-07-30 11:14:23 -0400

    created game logic through functions

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
+//array containing cards for game
 var cards = ["queen","queen","king","king"];
+//array for cards that get selected
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if(cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("Found a match!");
-	}else{
-		alert("Sorry, try again")
+//function to check for match
+var checkForMatch = function(){
+	if(cardsInPlay.length === 2){
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			alert("Found a match!");
+		}else{
+			alert("Sorry, try again")
+		}
 	}
 }
-console.log("User flipped " + cardsInPlay);
+//function for flipping cards and storing them
+var flipCard = function(cardId){
+	console.log("User flipped" + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+flipCard(0);
+flipCard(2);

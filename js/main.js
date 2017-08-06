62e81e707c0b18dff1f540a2b8296fb46c752a27
chris mears <chrismears@chriss-mbp.gateway.pace.com>
2017-08-06 14:03:38 -0400

    Updated card array to object

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 //array containing cards for game
-var cards = ["queen","queen","king","king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 //array for cards that get selected
 var cardsInPlay = [];
 //function to check for match
@@ -14,8 +35,10 @@ var checkForMatch = function(){
 }
 //function for flipping cards and storing them
 var flipCard = function(cardId){
-	console.log("User flipped" + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 }
 flipCard(0);
